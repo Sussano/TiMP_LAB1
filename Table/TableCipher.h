07f949b1549511db41f2e8dccc3eadbad14c9c80
Sussano <59088768+Sussano@users.noreply.github.com>
2023-11-20 15:50:09 +0300

    Create TableCipher.h

---
+++ Table/TableCipher.h
@@ -0,0 +1,11 @@
+#include <string>
+
+class TableCipher {
+public:
+    TableCipher(int columns);
+    std::string encrypt(const std::string& plaintext);
+    std::string decrypt(const std::string& ciphertext);
+
+private:
+    int numColumns;
+};
