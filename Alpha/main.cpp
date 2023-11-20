70abc3b7bbc208b45c125fba71f0b7980046b149
Sussano <59088768+Sussano@users.noreply.github.com>
2023-11-20 15:47:22 +0300

    Update main.cpp

--- Alpha/main.cpp
+++ Alpha/main.cpp
@@ -1,40 +1,50 @@
+#include <iostream>
+#include <cctype>
 #include "modAlphaCipher.h"
+#include <locale>
+using namespace std;
 
-modAlphaCipher::modAlphaCipher(const wstring& skey) {
-    for (unsigned i = 0; i < numAlpha.size(); i++) {
-        alphaNum[numAlpha[i]] = i;
-    }
-    key = convert(skey);
+bool isValid(const wstring& s)
+{
+    for(auto c : s)
+        if (!iswalpha(c) || !iswupper(c))
+            return false;
+    return true;
 }
 
-wstring modAlphaCipher::encrypt(const wstring& open_text) {
-    vector<int> work = convert(open_text);
-    for (unsigned i = 0; i < work.size(); i++) {
-        work[i] = (work[i] + key[i % key.size()]) % alphaNum.size();
+int main(int argc, char **argv)
+{
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+    wstring key;
+    wstring text;
+    unsigned op;
+    wcout << L"Cipher ready. Input key: ";
+    getline(wcin, key); // Считываем строку вместо wcin
+    if (!isValid(key)) {
+        wcerr << L"Key not valid\n";
+        return 1;
     }
-    return convert(work);
-}
-
-wstring modAlphaCipher::decrypt(const wstring& cipher_text) {
-    vector<int> work = convert(cipher_text);
-    for (unsigned i = 0; i < work.size(); i++) {
-        work[i] = (work[i] + alphaNum.size() - key[i % key.size()]) % alphaNum.size();
-    }
-    return convert(work);
-}
-
-vector<int> modAlphaCipher::convert(const wstring& s) {
-    vector<int> result;
-    for (auto c : s) {
-        result.push_back(alphaNum[c]);
-    }
-    return result;
-}
-
-wstring modAlphaCipher::convert(const vector<int>& v) {
-    wstring result;
-    for (auto i : v) {
-        result.push_back(numAlpha[i]);
-    }
-    return result;
+    wcout << L"Key loaded\n";
+    modAlphaCipher cipher(key);
+    do {
+        wcout << L"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
+        wcin >> op;
+        if (op > 2) {
+            wcout << L"Illegal operation\n";
+        } else if (op > 0) {
+            wcin.ignore(); // Сбрасываем символ новой строки в буфере
+            wcout << L"Cipher ready. Input text: ";
+            getline(wcin, text); // Считываем строку вместо wcin
+            if (isValid(text)) {
+                if (op == 1) {
+                    wcout << L"Encrypted text: " << cipher.encrypt(text) << endl;
+                } else {
+                    wcout << L"Decrypted text: " << cipher.decrypt(text) << endl;
+                }
+            } else {
+                wcout << L"Operation aborted: invalid text\n";
+            }
+        }
+    } while (op != 0);
+    return 0;
 }
